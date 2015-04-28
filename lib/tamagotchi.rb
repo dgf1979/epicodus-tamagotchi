8d7afa8866aec68b2a57f5e97f3e9a32d481b89b
Drew & Ian <student@epicodus.com>
2015-04-28 13:46:28 -0700

    add method logic to pass unit test for name, sleep, food, and activity

--- lib/tamagotchi.rb
+++ lib/tamagotchi.rb
@@ -1,21 +1,29 @@
 class Tamagotchi
+  @name
+  @food_level
+  @sleep_level
+  @activity_level
+
   define_method(:initialize) do |name|
-    #code
+    @name = name
+    @food_level = 10
+    @sleep_level = 10
+    @activity_level = 10
   end
 
   define_method(:name) do
-
+    @name
   end
 
   define_method(:food_level) do
-
+    @food_level
   end
 
   define_method(:sleep_level) do
-
+    @sleep_level
   end
 
   define_method(:activity_level) do
-
+    @activity_level
   end
 end
