e980d211ca103929462156520b8b3a7af6092999
Drew & Ian <student@epicodus.com>
2015-04-28 13:43:27 -0700

    add failing unit test for class init and initial values

---
+++ lib/tamagotchi.rb
@@ -0,0 +1,21 @@
+class Tamagotchi
+  define_method(:initialize) do |name|
+    #code
+  end
+
+  define_method(:name) do
+
+  end
+
+  define_method(:food_level) do
+
+  end
+
+  define_method(:sleep_level) do
+
+  end
+
+  define_method(:activity_level) do
+
+  end
+end
